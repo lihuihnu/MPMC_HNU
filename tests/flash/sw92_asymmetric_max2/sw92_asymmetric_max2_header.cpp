db09af97c06cd0986f87c9f5906c1f9fc76d70fc
lihuihnu <346848032lihui@gmail.com>
2026-09-10 19:16:54 +0800

    test(flash): preserve zero TPD tolerance in Gate 3B.3

--- tests/flash/sw92_asymmetric_max2/sw92_asymmetric_max2_header.cpp
+++ tests/flash/sw92_asymmetric_max2/sw92_asymmetric_max2_header.cpp
@@ -2,5 +2,7 @@
 
 bool sw92_asymmetric_max2_header() {
     mpmc::flash::Sw92AsymmetricMax2Options options;
-    return options.selection.max_pair_attempts > 0;
+    return options.selection.max_pair_attempts > 0 &&
+           mpmc::flash::detail::sw92_max2_effective_tpd_tolerance(0.0, 0.0) == 0.0 &&
+           mpmc::flash::detail::sw92_max2_effective_tpd_tolerance(0.0, 0.25) == 0.25;
 }
