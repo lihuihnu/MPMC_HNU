259ae0f13c537c6b4199c76ff49f229c79605fc9
lihuihnu <346848032lihui@gmail.com>
2026-09-14 15:48:40 +0800

    test(android): expose debuggable WebView for Product Shell DOM audit

--- products/pt_android/shell/android-src/org/mpmc/ptandroid/MainActivity.java
+++ products/pt_android/shell/android-src/org/mpmc/ptandroid/MainActivity.java
@@ -1,11 +1,16 @@
 package org.mpmc.ptandroid;
 
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public final class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        if ((getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
         registerPlugin(MpmcPtPlugin.class);
         super.onCreate(savedInstanceState);
     }
