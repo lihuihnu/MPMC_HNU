52d5c118fb01cf9171feda19be36a355c32dd08a
lihuihnu <346848032lihui@gmail.com>
2026-09-17 21:10:36 +0800

    test(cpa): inject target pressure into full certificate

--- tests/flash/cpa_association_continuation_audit/certificate_roundoff_audit.cpp
+++ tests/flash/cpa_association_continuation_audit/certificate_roundoff_audit.cpp
@@ -1,6 +1,6 @@
 #include "certificate_roundoff_auto.hpp"
 
-#define certify certify_with_pressure_roundoff_auto
+#define certify(...) certify_with_full_roundoff_auto(request.pressure_pa, __VA_ARGS__)
 #define main mpmc_cpa_association_certificate_base_main
 #include "certificate_audit.cpp"
 #undef main
