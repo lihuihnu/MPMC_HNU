ac1a942c3ecf807bd7a65e55bb58b7243151437f
lihuihnu <346848032lihui@gmail.com>
2026-09-18 20:49:17 +0800

    test(cpa): expose stability header contract

--- tests/flash/cpa_stability/cpa_stability_headers.cpp
+++ tests/flash/cpa_stability/cpa_stability_headers.cpp
@@ -1,5 +1,5 @@
 #include <mpmc/flash/cpa_stability.hpp>
 
-bool cpa_stability_public_header_self_contained() {
-    return mpmc::flash::cpa_pt_stability_convention[0] != '\0';
+int main() {
+    return mpmc::flash::cpa_pt_stability_convention[0] != '\0' ? 0 : 1;
 }
