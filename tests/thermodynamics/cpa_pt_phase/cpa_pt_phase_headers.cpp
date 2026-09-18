434eb087d8040072bfd4fb220f936efa61595a5a
lihuihnu <346848032lihui@gmail.com>
2026-09-18 19:39:36 +0800

    test(cpa): expose PT phase header contract

--- tests/thermodynamics/cpa_pt_phase/cpa_pt_phase_headers.cpp
+++ tests/thermodynamics/cpa_pt_phase/cpa_pt_phase_headers.cpp
@@ -1,5 +1,5 @@
 #include <mpmc/thermodynamics/cpa_pt_phase.hpp>
 
-bool cpa_pt_phase_public_header_self_contained() {
-    return mpmc::thermodynamics::cpa_pt_convention.size() > 0U;
+int main() {
+    return mpmc::thermodynamics::cpa_pt_convention.size() > 0U ? 0 : 1;
 }
