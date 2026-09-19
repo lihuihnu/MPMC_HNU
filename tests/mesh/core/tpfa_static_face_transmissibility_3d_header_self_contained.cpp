cc1679d34ac6b6edf8ee13f4a6317f999281e564
lihuihnu <346848032lihui@gmail.com>
2026-09-19 14:25:00 +0800

    test(mesh): compile static TPFA face transmissibility header standalone

---
+++ tests/mesh/core/tpfa_static_face_transmissibility_3d_header_self_contained.cpp
@@ -0,0 +1,6 @@
+#include <mpmc/mesh/tpfa_static_face_transmissibility_3d.hpp>
+
+#include <type_traits>
+
+static_assert(std::is_trivially_copyable_v<
+              mpmc::mesh::TpfaStaticFaceTransmissibility3D>);
