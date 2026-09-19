a1ab2b9aef01f281ae1ef8890fbae6784be87ac1
lihuihnu <346848032lihui@gmail.com>
2026-09-19 17:52:07 +0800

    fix(discretization): preserve geometry classifier API

--- modules/discretization/include/mpmc/discretization/transmissibility_admissibility_3d.hpp
+++ modules/discretization/include/mpmc/discretization/transmissibility_admissibility_3d.hpp
@@ -41,7 +41,7 @@ struct TransmissibilityGeometryAdmissibility3D {
 
 
 [[nodiscard]] inline TransmissibilityGeometryAdmissibility3D
-classify_internal_face_face_connection_geometry(
+classify_internal_face_transmissibility_geometry(
     const mpmc::mesh::CellFaceGeometricOperator3D& geometry,
     mpmc::mesh::LocalIndex face,
     TransmissibilityGeometryAdmissibilityPolicy3D policy) {
@@ -409,7 +409,7 @@ classify_internal_face_transmissibility_admissibility(
         geometry_policy,
     KOrthogonalityAdmissibilityPolicy3D k_policy) {
     const auto geometry_result =
-        classify_internal_face_face_connection_geometry(
+        classify_internal_face_transmissibility_geometry(
             geometry,
             face,
             geometry_policy);
