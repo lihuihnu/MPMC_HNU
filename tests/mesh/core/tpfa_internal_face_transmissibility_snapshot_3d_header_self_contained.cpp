abceacd6ee423373989b1c0b480e6d4aa5002d42
lihuihnu <346848032lihui@gmail.com>
2026-09-19 14:39:39 +0800

    test(mesh): compile gated TPFA snapshot header standalone

---
+++ tests/mesh/core/tpfa_internal_face_transmissibility_snapshot_3d_header_self_contained.cpp
@@ -0,0 +1,8 @@
+#include <mpmc/mesh/tpfa_internal_face_transmissibility_snapshot_3d.hpp>
+
+#include <type_traits>
+
+static_assert(std::is_copy_constructible_v<
+              mpmc::mesh::TpfaInternalFaceTransmissibilitySnapshot3D>);
+static_assert(!std::is_default_constructible_v<
+              mpmc::mesh::TpfaInternalFaceTransmissibilitySnapshot3D>);
