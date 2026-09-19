eff369728df28007f62f3059438367ae1f1882e3
lihuihnu <346848032lihui@gmail.com>
2026-09-19 17:26:25 +0800

    test(mesh): remove migrated TPFA header test

--- tests/mesh/core/tpfa_half_transmissibility_3d_header_self_contained.cpp
+++
@@ -1,6 +0,0 @@
-#include <mpmc/mesh/tpfa_half_transmissibility_3d.hpp>
-
-#include <type_traits>
-
-static_assert(std::is_trivially_copyable_v<
-              mpmc::mesh::TpfaAreaScaledHalfTransmissibility3D>);
