8a0fd795197b3eb226fc972f37a79c5c7ac3a0a1
lihuihnu <346848032lihui@gmail.com>
2026-09-19 17:26:22 +0800

    test(mesh): remove migrated TPFA header test

--- tests/mesh/core/tpfa_half_connection_3d_header_self_contained.cpp
+++
@@ -1,6 +0,0 @@
-#include <mpmc/mesh/tpfa_half_connection_3d.hpp>
-
-#include <type_traits>
-
-static_assert(std::is_trivially_copyable_v<
-              mpmc::mesh::TpfaHalfConnectionCoefficient3D>);
